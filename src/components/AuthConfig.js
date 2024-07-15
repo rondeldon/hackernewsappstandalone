6484a81833ad0adedfe50c3046a0085145f4cf21
Ronnie Brown <Ronnie.Brown@ceridian.com>
2024-07-15 07:48:22 -0400

    Updated redirect

--- src/components/AuthConfig.js
+++ src/components/AuthConfig.js
@@ -2,8 +2,8 @@ export const msalConfig = {
     auth: {
         clientId: "8da5bacc-fa67-4b80-9f80-f49b5fb3a40f",
         authority: "https://login.microsoftonline.com/05079e4c-6169-4ad3-8adb-ce8180c2208d",
-/*        redirectUri: "https://ambitious-desert-0ed06800f.4.azurestaticapps.net/", */
-        redirectUri: "http://localhost:3000", 
+        redirectUri: "https://ambitious-desert-0ed06800f.4.azurestaticapps.net/",
+/*        redirectUri: "http://localhost:3000", */
         cacheLocation: "sessionStorage",
         storeAuthStateInCookie: true,
     }
